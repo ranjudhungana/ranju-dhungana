0152f80379de94d47e8a6e92079af0c9ba213ba6
sandeep <sandeeppangeni17@gmail.com>
2018-03-26 17:21:27 +0545

    new project created and log app added to the project

---
+++ blog/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
+from django.shortcuts import redirect, get_object_or_404
+
+
+from .models import Post
+from .forms import PostForm
+
+
+def post_list(request):
+	posts=Post.objects.all()
+	return render(request, 'list.html', {'posts':posts})
+def post_new(request):
+	if request.method=="POST":
+		form = PostForm (request.POST)
+		if form .is_valid():
+			post=form.save(commit=False)
+			post.created=timezone.now()
+			post.save()
+			return redirect('post_list')
+	else:
+			form=PostForm()
+	return render(request, 'new_post.html', {'form': form})
+
+def post_detail(request, pk):
+	post=get_object_or_404(Post, pk = pk)
+	return render(request, 'post_detail.html',{'post': post})
+
+def edit_post(request,pk):
+	template='new_post.html'
+	post=get_object_or_404(Post,pk=pk)
+
+	if request.method=='POST':
+		form =PostForm(request.POST, instance=post)
+		if form. is_valid():
+			form.save()
+			return redirect('/')
+			
+	else:
+		form= PostForm(instance=post)
+
+	context={
+	'form':form,
+	'post':post
+	}
+
+	return render(request, template , context)
+
+def post_delete(request,pk):
+	post=get_object_or_404(Post, pk=pk)
+	post.delete()
+	return HttpResponse("deleted")
+
